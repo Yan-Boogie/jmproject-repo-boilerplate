622a03738bc1bde7c3fe0d230a35fc8300c51a98
Ian Lai <ian.lai@houzz.com>
2024-04-24 00:33:03 +0800

    feat: build bundler and linters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { someUtils } from './another.js';
+import { someUtils } from './another';
 
 document.body.innerHTML += '<p class="text-red-500">This is a paragraph added by JavaScript from src/index.js</p>';
 document.body.innerHTML += someUtils();
